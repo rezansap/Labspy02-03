8f1ea6a2a750b0e6b07b0230d3887a3a724c013a
rezansap <srezaputra14@gmail.com>
2021-11-12 10:08:38 +0700

    menambahahkan file python latihan1

---
+++ Latihan1.py
@@ -0,0 +1,6 @@
+print("Tampilkan n bilangan acak yang lebih kecil dari 0.5")
+
+jumlah = int(input("Masukkan jumlah n: "))
+import random
+for i in range(jumlah):
+    print("data ke", i+1,"=",(random.uniform(0.1,0.5)))
